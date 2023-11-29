237f083cdf1fc95e15649d8441b8137620e3971c
AlexSkrll <alexskrll25@gmail.com>
2023-11-29 22:26:37 +0200

    Initial commit

---
+++ Assets/Scripts/Teza/AnimationSynchronizer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class AnimationSynchronizer : MonoBehaviour
+{
+    public GameObject tezaBodyObject; 
+    public GameObject tezaArmObject; 
+
+    private Animator tezaBody; 
+    private Animator tezaArm; 
+    
+    void Start()
+    {
+        tezaBody = tezaBodyObject.GetComponent<Animator>();
+        tezaArm = tezaArmObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        SyncAnimations();
+    }
+
+    void SyncAnimations()
+{
+   
+    float moveX = tezaBody.GetFloat("moveX");
+    float moveY = tezaBody.GetFloat("moveY");
+    bool bodyIsMoving = tezaBody.GetBool("isMoving");
+    float timeSinceLastMovement = tezaBody.GetFloat("timeSinceLastMovement");
+
+    tezaArm.SetFloat("armmoveX", moveX);
+    tezaArm.SetFloat("armmoveY", moveY);
+    tezaArm.SetBool("isMoving", bodyIsMoving);
+    tezaArm.SetFloat("timeSinceLastMovement", timeSinceLastMovement);
+}
+}
